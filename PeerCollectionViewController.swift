df22475d16a17bce824617639e034a69ca818bec
Kate Willison <kt.willison@gmail.com>
2015-12-04 22:04:13 -0800

    started implementing peer connectivity

---
+++ PeerCollectionViewController.swift
@@ -0,0 +1,209 @@
+//
+//  PeerCollectionViewController.swift
+//  WineNotes
+//
+//  Created by Katie Willison on 12/4/15.
+//  Copyright © 2015 Kate Willison. All rights reserved.
+//  Basic outline of multipeer connectivity setup adapted from
+//  http://radar.oreilly.com/2014/09/multipeer-connectivity-on-ios-8-with-swift.html
+
+import UIKit
+import MultipeerConnectivity
+
+let reuseIdentifier = "reuseIdentifier"
+
+class PeerCollectionViewController: UICollectionViewController, MCBrowserViewControllerDelegate, MCSessionDelegate {
+    
+    let serviceType = "WineNotes"
+    
+    var browserVC : MCBrowserViewController!
+    var advertiserAssistant : MCAdvertiserAssistant!
+    var session : MCSession!
+    var myPeerID: MCPeerID!
+    
+    @IBOutlet var chatView: UITextView!
+    @IBOutlet var messageField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Get peerID from device name
+        myPeerID = MCPeerID(displayName: UIDevice.currentDevice().name)
+        session = MCSession(peer: myPeerID)
+        session.delegate = self
+        
+        // create the browser viewcontroller with a unique service name
+        browserVC = MCBrowserViewController(serviceType:serviceType,
+            session:session)
+        
+        browserVC.delegate = self;
+        
+        advertiserAssistant = MCAdvertiserAssistant(serviceType:serviceType,
+            discoveryInfo:nil, session:session)
+        
+        // start advertising the service
+        advertiserAssistant.start()
+    }
+    
+    func sendRating(rating : WineReview) {
+        // Send the given rating to all connected peers
+        
+        let msg = self.messageField.text.dataUsingEncoding(NSUTF8StringEncoding,
+            allowLossyConversion: false)
+        
+        var error : NSError?
+        
+        session.sendData(msg, toPeers: session.connectedPeers,
+            withMode: MCSessionSendDataMode.Unreliable, error: &error)
+        
+        if error != nil {
+            print("Error sending data: \(error?.localizedDescription)")
+        }
+        
+        self.updateResults(self.messageField.text, fromPeer: myPeerID)
+        
+        self.messageField.text = ""
+    }
+    
+    func updateResults(text : String, fromPeer peerID: MCPeerID) {
+        // Appends some text to the chat view
+        
+        // If this peer ID is the local device's peer ID, then show the name
+        // as "Me"
+        var name : String
+        
+        switch peerID {
+        case peerID:
+            name = "Me"
+        default:
+            name = peerID.displayName
+        }
+        
+        // Add the name to the message and display it
+        let message = "\(name): \(text)\n"
+        self.chatView.text = self.chatView.text + message
+        
+    }
+    
+    @IBAction func showBrowser(sender: UIButton) {
+        // Show the browser view controller
+        self.presentViewController(browserVC, animated: true, completion: nil)
+    }
+    
+    func browserViewControllerDidFinish(
+        browserViewController: MCBrowserViewController)  {
+            // Called when the browser view controller is dismissed (ie the Done
+            // button was tapped)
+            
+            self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func browserViewControllerWasCancelled(
+        browserViewController: MCBrowserViewController)  {
+            // Called when the browser view controller is cancelled
+            
+            self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func session(session: MCSession, didReceiveData data: NSData, fromPeer peerID: MCPeerID) {
+            // Called when a peer sends an NSData to us
+            
+            // This needs to run on the main queue
+            dispatch_async(dispatch_get_main_queue()) {
+                
+                var msg = NSString(data: data, encoding: NSUTF8StringEncoding)
+        
+                self.updateChat(msg, fromPeer: peerID)
+            }
+    }
+    
+    // The following methods do nothing, but the MCSessionDelegate protocol
+    // requires that we implement them.
+    func session(session: MCSession,
+        didStartReceivingResourceWithName resourceName: String,
+        fromPeer peerID: MCPeerID, withProgress progress: NSProgress)  {
+            // Called when a peer starts sending a file to us
+    }
+    
+    func session(session: MCSession,
+        didFinishReceivingResourceWithName resourceName: String,
+        fromPeer peerID: MCPeerID,
+        atURL localURL: NSURL, withError error: NSError?)  {
+            // Called when a file has finished transferring from another peer
+    }
+    
+    func session(session: MCSession, didReceiveStream stream: NSInputStream,
+        withName streamName: String, fromPeer peerID: MCPeerID)  {
+            // Called when a peer establishes a stream with us
+    }
+    
+    func session(session: MCSession, peer peerID: MCPeerID,
+        didChangeState state: MCSessionState)  {
+            // Called when a connected peer changes state (for example, goes offline)
+            
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 0
+    }
+
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath)
+    
+        // Configure the cell
+    
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(collectionView: UICollectionView, shouldHighlightItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(collectionView: UICollectionView, shouldSelectItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(collectionView: UICollectionView, shouldShowMenuForItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(collectionView: UICollectionView, canPerformAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) -> Bool {
+        return false
+    }
+
+    override func collectionView(collectionView: UICollectionView, performAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) {
+    
+    }
+    */
+
+}
